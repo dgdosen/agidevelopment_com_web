de5d8ce527c957c4032b631d52d52cc04dce8309
Daniel Dosen <dgdosen@gmail.com>
2018-08-29 16:44:51 -0700

    tweak site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: '/agidevelopment_com_web',
   siteMetadata: {
-    title: 'AgI Development',
+    title: 'AGI Development',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
